4a932fb366ec94531ba263794654b3266542b080
omrinao <38496755+omrinao@users.noreply.github.com>
2019-06-14 19:24:54 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,268 @@
+var express = require("express");
+var app = express();
+var path = require('path');
+var DButilsAzure = require('./DButils');
+const jwt = require("jsonwebtoken");
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
+var userRouter = require("./user");
+
+
+var secret = "Everyonelovesberlin6";
+
+var port = 3000;
+app.listen(port, function () {
+    console.log('app is listening on port ' + port);
+});
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+/**
+ * this function check the token for the user.
+ */
+app.use("/private", (req, res,next) => {
+    const token = req.header("x-auth-token");
+    // no token
+    if (!token) res.status(401).send("Access denied. No token provided.");
+    // verify token
+    try {
+        const decoded = jwt.verify(token, secret);
+        req.decoded = decoded;
+        next(); //move on to the actual function
+    } catch (exception) {
+        res.status(400).send("Invalid token.");
+    }
+});
+app.use("/private/user",userRouter);
+
+
+/**
+ * function that register a new user
+ */
+app.post('/Register', (req, res) => {
+    async function Reg(){
+        try{
+            var username = req.body.UserName
+            var password = req.body.Password
+            var fName = req.body.FirstName
+            var lName = req.body.LastName
+            var city = req.body.City
+            var country = req.body.Country
+            var FOI = req.body.FieldOfInterest
+            var question = req.body.Question
+            var answer = req.body.Answer
+            var checkUser = await DButilsAzure.execQuery("SELECT UserName FROM tbl_Users WHERE UserName='" + username + "'")
+            
+            if (checkUser.length > 0)
+                res.send("User name already exist")
+            else if (checkUser.length > 0 || question.length != answer.length || FOI.length < 2)
+                res.send(err)
+            else{
+                await DButilsAzure.execQuery("INSERT INTO tbl_Users (UserName, Password, FirstName, LastName, City, Country) VALUES ('" + username + "', '" + password + "', '" + fName +"', '" + lName +"', '" + city +"', '" + country +"')")
+    
+                for (var i = 0; i < question.length; i++){
+                    await DButilsAzure.execQuery("INSERT INTO tbl_User_RecoveryQA (UserName, Question, Answer) VALUES ('" + username + "', '" + question[i] + "', '" + answer[i] + "')")
+                }
+                for (var i = 0 ; i < FOI.length; i++)
+                    await DButilsAzure.execQuery("INSERT INTO tbl_User_FOI (UserName, FieldOfInterest) VALUES ('" + username + "', '" + FOI[i] + "')")
+            }
+            res.send("success")
+        }
+        catch(err){
+            res.send(err)
+        }
+    }
+    Reg();
+});
+
+
+/**
+ * this function checks the user id and password and assign a token to him
+ */
+app.post("/login", (req, res) => {
+    async function login(){
+        try{
+            var username = await req.body.UserName
+            var password = await req.body.Password
+            var result = await DButilsAzure.execQuery("SELECT Password FROM tbl_Users WHERE UserName='" + username + "'")
+            var strWithoutSpace = result[0].Password.replace(/\s*$/,'');
+            if (strWithoutSpace == password){
+                payload = { UserName: username };
+	            options = { expiresIn: "2h" };
+	            const token = jwt.sign(payload, secret, options);
+	            res.send(token);
+            }
+            else{
+                res.send("Wrong User Name Or Password")
+            }
+        }
+        catch(err){
+            res.send(err)
+        }
+    }
+    login();
+});
+
+
+/**
+ * this function restores a user password according to QA recovery verification
+ */
+app.post("/RestorePassword", (req, res) => {
+    async function restore(){
+        try{
+            var username = await req.body.UserName
+            var question = req.body.Question
+            var answer = req.body.Answer
+            var checkUser = await DButilsAzure.execQuery("SELECT UserName FROM tbl_Users WHERE UserName='" + username + "'")
+            
+            if (checkUser.length == 0)
+                res.send("User name does not exist")
+
+            var result = await DButilsAzure.execQuery("SELECT Question, Answer FROM tbl_User_RecoveryQA WHERE UserName='" + username + "'")
+            var qWithoutSpace = "";
+            var aWithoutSpace = "";
+            var flag = true;
+            for (i = 0; i < result.length && flag; i++){
+                qWithoutSpace = result[i].Question.replace(/\s*$/,'');
+                aWithoutSpace = result[i].Answer.replace(/\s*$/,'');
+                if (qWithoutSpace != question[i] || aWithoutSpace != answer[i]){
+                    flag = false;
+                }
+            }
+            if (flag){
+                var pass = await DButilsAzure.execQuery("SELECT Password FROM tbl_Users WHERE UserName='" + username + "'")
+                res.send(pass[0].Password.replace(/\s*$/,''))
+            }
+            else{
+                res.send("Wrong answers")
+            }
+        }
+        catch(err){
+            res.send(err)
+        }
+    }
+    restore();
+});
+
+
+/**
+ * function that returns a given POI details
+ */
+app.get('/getPOI_info/:POI', function(req, res){
+    async function getPOIf(){
+        try{
+            var r = await req.params.POI
+            result = await DButilsAzure.execQuery("SELECT * FROM tbl_POI WHERE POIName='" + r + "'")
+            if (result.length == 0)
+                res.send("No such POI")
+            for (var i = 0; i < result.length; i++){
+                result[i].POIName = result[i].POIName.replace(/\s*$/,'');
+                result[i].CategoryName = result[i].CategoryName.replace(/\s*$/,'');
+                result[i].Description = result[i].Description.replace(/\s*$/,'');
+                result[i].Rank = result[i].Rank * 20 + "%";
+            }
+            var reviews = await DButilsAzure.execQuery("SELECT TOP 2 (Review) FROM tbl_User_Review WHERE POIName='" + r + "' ORDER BY Date DESC")
+            res.send(result.concat(reviews))
+        }
+        catch(err){
+            res.send(err)
+        }
+    }
+    getPOIf();
+})
+
+
+/**
+ * function that returns all POI's belongs to a given category
+ */
+app.get('/getCatPOI/:category', function(req, res){
+    var r = req.params.category
+    DButilsAzure.execQuery("SELECT * FROM tbl_POI WHERE CategoryName='" + r + "'")
+    .then(function(result){
+        if (result.length == 0)
+            res.send("No POI match to this category")
+        
+        else
+        res.send(result)
+    })
+    .catch(function(err){
+        res.send(err)
+    })
+})
+
+
+/**
+ * function that returns a given user verification questions
+ */
+app.get('/getQuestions/:user', function(req, res){
+    async function getQuestionsf(){
+        try{
+            var username = await req.params.user
+            var queryResult = await DButilsAzure.execQuery("SELECT Question FROM tbl_User_RecoveryQA WHERE UserName='" + username + "'")
+            if (queryResult.length == 0)
+                res.send("No such user name")
+            for (var i = 0; i < queryResult.length; i++){
+                queryResult[i].Question = queryResult[i].Question.replace(/\s*$/,'');
+            }
+            res.send(queryResult)
+        }
+        catch(err){
+            res.send(err)
+        }
+    }
+    getQuestionsf();
+})
+
+
+/**
+ * function that returns random POI's 
+ */
+app.get('/GetRandomPOI/:minRank', (req, res) => {
+	async function getRecomendedPOI(){
+        try{
+            var minRank = await req.params.minRank
+            var POIs = await DButilsAzure.execQuery("SELECT * FROM tbl_POI WHERE Rank >= '" + minRank + "'")
+            var counter = 0
+            var toSend = []
+            if (POIs.length < 3){
+                for (var i = 0; i < POIs.length; i++){
+                    toSend[counter] = POIs[i]
+                    counter++
+                }
+            }
+            else
+            {
+                var rand = Math.random();
+                var fhinished = false
+                while (!fhinished){
+                    for (var i = 0; !fhinished && counter < 3 &&  i < POIs.length; i++){
+                        if (rand > 0.5){
+                            if (toSend.includes(POIs[i])){
+                                continue
+                            }
+                            toSend[counter] = POIs[i];
+                            counter++
+                            if (counter == 3){
+                                fhinished = true
+                            }
+                        }
+                        rand = Math.random();
+                    }
+                }
+            }
+            res.send(toSend)
+    }
+        catch(err){
+            res.send(err)
+        }
+    }
+    getRecomendedPOI();
+    
+});
---
+++ user.js
@@ -0,0 +1,254 @@
+var express = require("express");
+var router = express.Router();
+var DButilsAzure = require('./DButils');
+
+
+/**
+ * function that returns a given user FOI's details
+ */
+router.get('/getFOI/:user', function(req, res){
+    async function getFoif(){
+        try{
+            var r = req.params.user
+            var checkUser = await DButilsAzure.execQuery("SELECT UserName FROM tbl_Users WHERE UserName='" + username + "'")
+                    
+                if (checkUser.length == 0)
+                    res.send("User name does not exist")
+
+            var result = await DButilsAzure.execQuery("SELECT FieldOfInterest FROM tbl_User_FOI WHERE UserName='" + r + "'")
+            if (result.length == 0)
+                res.send("No FOI for this user name")
+            for (var i = 0; i < result.length; i++)
+                result[i].FieldOfInterest = result[i].FieldOfInterest.replace(/\s*$/,'');
+            res.send(result)
+        }
+        catch(err){
+            res.send(err)
+        }
+    }
+    getFoif();
+    
+})
+
+
+/**
+ * function that saves a POI for a user
+ */
+router.post('/savePOI', (req, res) => {
+    async function poiSave(){
+        try{
+            var username = req.body.UserName
+            var poiname = req.body.POIName
+            var checkUser = await DButilsAzure.execQuery("SELECT UserName FROM tbl_Users WHERE UserName='" + username + "'")
+            
+            if (checkUser.length == 0)
+                res.send("User name does not exist")
+
+            var result = await DButilsAzure.execQuery("SELECT * FROM tbl_Saved_POI WHERE UserName='" + username + "' AND POIName='" + poiname + "'")
+            if (result.length == 0){
+                result = await DButilsAzure.execQuery("INSERT INTO tbl_Saved_POI (UserName, POIName) VALUES ('" + username + "', '" + poiname + "')")
+                res.send("success")
+            }
+            else{
+                res.send("POI Already Saved")
+            }
+        }
+        catch(err){
+            res.send(err)
+        }
+    }
+    poiSave();
+});
+
+
+/**
+ * function that returns recommended POI's for a given user, according to his FOI's, number of views and minimum rank 
+ */
+router.get('/getRecomendedPOI/:user', (req, res) => {
+	async function getRecomendedPOI(){
+        try{
+            var username = await req.params.user
+            var checkUser = await DButilsAzure.execQuery("SELECT UserName FROM tbl_Users WHERE UserName='" + username + "'")
+            
+            if (checkUser.length == 0)
+                res.send("User name does not exist")
+
+            var queryFOIResult = await DButilsAzure.execQuery("SELECT FieldOfInterest FROM tbl_User_FOI WHERE UserName='" + username + "'")
+            var strWithoutSpace = "";
+            var finalQuery = "";
+            for (var i = 0; i < queryFOIResult.length; i++){
+                strWithoutSpace = queryFOIResult[i].FieldOfInterest.replace(/\s*$/,'');
+                var queryPOIResult = await DButilsAzure.execQuery("SELECT * FROM tbl_POI WHERE (CategoryName='" + strWithoutSpace +
+             "' AND NumOfViews >= 10000 AND Rank >= 4.0)")
+                if (finalQuery.length == 0){
+                    finalQuery = queryPOIResult;
+                }
+                else if (queryPOIResult.length != 0 && finalQuery.length != 0){
+                        finalQuery = finalQuery.concat(queryPOIResult)
+                }
+            }
+            for (var i = 0; i < finalQuery.length; i++){
+                finalQuery[i].CategoryName = finalQuery[i].CategoryName.replace(/\s*$/,'');
+                finalQuery[i].POIName = finalQuery[i].POIName.replace(/\s*$/,'');
+            }
+            res.send(finalQuery)
+        }
+        catch(err){
+            res.send(err)
+        }
+    }
+    getRecomendedPOI();
+    
+});
+
+
+/**
+ * function that return the saved POI of a user
+ */
+router.get('/GetPOISaved/:user', (req, res) => {
+    async function getSavedPOI(){
+        try{
+            var username = req.params.user
+            var checkUser = await DButilsAzure.execQuery("SELECT UserName FROM tbl_Users WHERE UserName='" + username + "'")
+            
+            if (checkUser.length == 0)
+                res.send("User name does not exist")
+
+            var result = await DButilsAzure.execQuery("SELECT POIName FROM tbl_Saved_POI WHERE UserName='" + username + "'")
+            for (var i = 0; i < result.length; i++){
+                result[i].POIName = result[i].POIName.replace(/\s*$/,'');
+            }
+            res.send(result)
+        }
+        catch(err){
+            res.send(err)
+        }
+    }
+    getSavedPOI();
+});
+
+
+/**
+ * function that remove POI from the user POI saved list
+ */
+router.post('/RemovePOI', (req, res) => {
+
+    async function RemoveSavedPOI(){
+        try{
+            var username = req.body.UserName
+            var checkUser = await DButilsAzure.execQuery("SELECT UserName FROM tbl_Users WHERE UserName='" + username + "'")
+            
+            if (checkUser.length == 0)
+                res.send("User name does not exist")
+
+            var poiname = req.body.POIName
+            if (poiname == "All"){
+                await DButilsAzure.execQuery("DELETE FROM tbl_Saved_POI WHERE UserName='" + username +"'")
+            }
+            else{
+                await DButilsAzure.execQuery("DELETE FROM tbl_Saved_POI WHERE UserName='" + username + "' AND POIName='" + poiname + "'")
+            }      
+            res.send("success")
+        }
+        catch(err){
+            res.send(err)
+        }
+    }
+    RemoveSavedPOI();
+});
+
+
+/**
+ * function that clears the savedPOI of a user and inserting the new POIs.
+ */
+router.post('/UpdatePOIList', (req, res) => {
+
+    async function UpdatePOI(){
+        try{
+            var username = req.body.UserName
+            var checkUser = await DButilsAzure.execQuery("SELECT UserName FROM tbl_Users WHERE UserName='" + username + "'")
+                    
+                if (checkUser.length == 0)
+                    res.send("User name does not exist")
+
+            var POIs = req.body.POIName
+            var POIArr = POIs.split(',')
+            result = DButilsAzure.execQuery("DELETE FROM tbl_Saved_POI WHERE UserName='" + username +"'")
+            for (var i = 0 ; i < POIArr.length; i++)
+                await DButilsAzure.execQuery("INSERT INTO tbl_Saved_POI (UserName, POIName) VALUES ('" + username + "', '" + POIArr[i] + "')")
+            res.send("success")
+        }
+        catch(err){
+            res.send(err)
+        }
+    }
+    UpdatePOI();
+});
+
+
+/**
+ * function that saves a user review for a given POI and update rank for POI after review
+ */
+router.post('/SaveUsersReview', (req, res) => {
+    async function saveReview(){
+        try{
+            var newRank = 0;
+            var username = req.body.UserName
+            var poiname = req.body.POIName
+            var Rank = req.body.Rank
+            var Review = req.body.Description
+            var newNumOfReviews = 0;
+            var reviewDate = new Date();
+            var date = reviewDate.getFullYear()+'-'+(reviewDate.getMonth()+1)+'-'+reviewDate.getDate();
+            var alreadyReviewd = await DButilsAzure.execQuery("SELECT * FROM tbl_User_Review WHERE POIName='" + poiname + "' AND UserName='" + username + "'");
+            
+
+            if (alreadyReviewd.length > 0){//updating the review by the user.
+                
+                //getting the old rank of user to the new avgRank culc.
+                var oldUserRank = await DButilsAzure.execQuery("SELECT * FROM tbl_User_Review WHERE POIName='" + poiname + "' AND UserName='" + username + "'");
+                var numOfReviews = await DButilsAzure.execQuery("SELECT * FROM tbl_User_Review WHERE POIName='" + poiname + "'");
+                var oldRank = 0
+                for (var i = 0 ; i < numOfReviews.length; i = i + 1){
+                    oldRank = oldRank + numOfReviews[i].Rank
+                }
+                //update the user review.
+                await DButilsAzure.execQuery("UPDATE tbl_User_Review SET Rank=" + Rank + ",Review='" + Review + "',Date='" + date + "' WHERE POIName='" + poiname + "' AND UserName='" + username + "'");
+
+                            
+                newRank = ((oldRank - oldUserRank[0].Rank) * (numOfReviews.length - 1) + parseFloat(Rank))/(numOfReviews.length);
+                //update the POI table with the new avgRank
+                await DButilsAzure.execQuery("UPDATE tbl_POI SET Rank=" + newRank + ",NumOfViews='" + numOfReviews.length +"'  WHERE POIName='" + poiname + "'");
+                res.send("Update success")
+            }
+            else{   //did not reviewed by the user.
+                var numOfReviews = await DButilsAzure.execQuery("SELECT * FROM tbl_User_Review WHERE POIName='" + poiname + "'");
+                if (numOfReviews.length > 0){//there are reviews on the POI.
+                    var oldRank = 0
+                    for (var i = 0 ; i < numOfReviews.length; i = i + 1){
+                        oldRank = oldRank + numOfReviews[i].Rank
+                    }
+                    newRank = (oldRank * (numOfReviews.length) + parseFloat(Rank))/(numOfReviews.length + 1);
+                    newNumOfReviews = numOfReviews.length + 1
+
+                    //inserting the review of the user to the user review table.
+                    result = await DButilsAzure.execQuery("INSERT INTO tbl_User_Review (UserName,POIName,Rank,Review,Date) VALUES ('" + username + "', '" + poiname + "', '" + Rank + "', '"  + Review +"', '" + date + "')");
+
+                    //update the POI table with the new avgRank.
+                    await DButilsAzure.execQuery("UPDATE tbl_POI SET Rank=" + newRank + ",NumOfViews='" + newNumOfReviews +"'  WHERE POIName='" + poiname + "'");
+                }
+                else{//first review of the POI
+                    result = await DButilsAzure.execQuery("INSERT INTO tbl_User_Review (UserName,POIName,Rank,Review,Date) VALUES ('" + username + "', '" + poiname + "', '" + Rank + "', '"  + Review +"', '" + date + "')");
+                    result = await DButilsAzure.execQuery("UPDATE tbl_POI SET Rank=" + newRank + ",NumOfViews='" + 1 +"'  WHERE POIName='" + poiname + "'");
+                }
+                res.send("success")
+            }
+        }
+        catch(err){
+            res.send(err)
+        }
+    }
+    saveReview();
+});
+
+module.exports = router;
